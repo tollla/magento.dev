f463663c4c457514ced09241c50592e774f1174e
tolla <anatolii.web@gmail.com>
2016-02-19 01:58:54 +0200

    Доступ к модулю через FrontEnd

---
+++ app/code/local/DS/News/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tolla-nout
+ * Date: 19.02.2016
+ * Time: 1:37
+ */
+class DS_News_IndexController extends Mage_Core_Controller_Front_Action
+{
+
+    public function indexAction()
+    {
+        echo '<h1>News</h1>';
+    }
+
+}
