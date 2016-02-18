07cb136a43d58babc64b0176f31d3782d33f80bd
tolla <anatolii.web@gmail.com>
2016-02-19 01:15:22 +0200

    Получение названия таблицы из config.xml

--- app/code/local/DS/News/sql/dsnews_setup/install-0.0.1.php
+++ app/code/local/DS/News/sql/dsnews_setup/install-0.0.1.php
@@ -5,16 +5,30 @@
  * Date: 18.02.2016
  * Time: 17:45
  */
-//die("module !!!".__FILE__.":".__LINE__);
 $installer = $this;
+$tableNews = $installer->getTable('dsnews/table_news');
+
+//die($tableNews);
+
 $installer->startSetup();
-$installer->run("
-    create table ds_news_entities(
-      `news_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-      `title` VARCHAR(255) NOT NULL,
-      `content` TEXT NOT NULL,
-      `created` DATETIME,
-      PRIMARY KEY (`news_id`)
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-");
+
+$installer->getConnection()->dropTable($tableNews);
+$table = $installer->getConnection()
+    ->newTable($tableNews)
+    ->addColumn('news_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'identity'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ))
+    ->addColumn('title', Varien_Db_Ddl_Table::TYPE_TEXT, '255', array(
+        'nullable'  => false,
+    ))
+    ->addColumn('content', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+        'nullable'  => false,
+    ))
+    ->addColumn('created', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
+        'nullable'  => false,
+    ));
+$installer->getConnection()->createTable($table);
+
 $installer->endSetup();
