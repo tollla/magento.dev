bcc07aa7bb6edb3fceca64534a9036511c2763d2
tolla <anatolii.web@gmail.com>
2016-02-19 17:52:48 +0200

    Использование блоков в админке

---
+++ app/code/local/DS/News/Block/Adminhtml/News.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tolla-nout
+ * Date: 19.02.2016
+ * Time: 17:21
+ */
+
+class DS_News_Block_Adminhtml_News extends Mage_Adminhtml_Block_Abstract
+{
+
+    public function _toHtml()
+    {
+        return '<h1>News Module: Admin section</h1>';
+    }
+
+}
--- app/code/local/DS/News/controllers/Adminhtml/NewsController.php
+++ app/code/local/DS/News/controllers/Adminhtml/NewsController.php
@@ -11,7 +11,12 @@ class DS_News_Adminhtml_NewsController extends Mage_Adminhtml_Controller_Action
 
     public function indexAction()
     {
-        echo '<h1>News Module: Admin section</h1>';
+        $this->loadLayout();
+        $this->_setActiveMenu('dsnews');
+
+        $contentBlock = $this->getLayout()->createBlock('dsnews/adminhtml_news');
+        $this->_addContent($contentBlock);
+        $this->renderLayout();
     }
 
 }
