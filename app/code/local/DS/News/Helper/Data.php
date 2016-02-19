6b529989d780f2c02b12ba1477f7cf8dd6a2910f
tolla <anatolii.web@gmail.com>
2016-02-19 17:18:29 +0200

    Создание админки для модуля

---
+++ app/code/local/DS/News/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tolla-nout
+ * Date: 19.02.2016
+ * Time: 16:46
+ */
+
+class DS_News_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ app/code/local/DS/News/controllers/Adminhtml/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tolla-nout
+ * Date: 19.02.2016
+ * Time: 16:42
+ */
+
+class DS_News_Adminhtml_NewsController extends Mage_Adminhtml_Controller_Action
+{
+
+    public function indexAction()
+    {
+        echo '<h1>News Module: Admin section</h1>';
+    }
+
+}
