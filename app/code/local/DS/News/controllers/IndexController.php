603e5fa90283fedccad928fb4cf184f33992044d
tolla <anatolii.web@gmail.com>
2016-02-19 10:59:58 +0200

    Показ новостей напрямую из базы данных

--- app/code/local/DS/News/controllers/IndexController.php
+++ app/code/local/DS/News/controllers/IndexController.php
@@ -10,6 +10,17 @@ class DS_News_IndexController extends Mage_Core_Controller_Front_Action
 
     public function indexAction()
     {
+        $resource = Mage::getSingleton('core/resource');
+        $read = $resource->getConnection('core_read');
+        $table = $resource->getTableName('dsnews/table_news');
+
+        $select = $read->select()
+            ->from($table, array('news_id', 'title', 'content', 'created'))
+            ->order('created DESC');
+
+        $news = $read->fetchAll($select);
+        Mage::register('news', $news);
+
         $this->loadLayout();
         $this->renderLayout();
     }
