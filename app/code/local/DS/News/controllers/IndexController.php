03eb1ff90b56a99dec7b7b9f2cebdaf2103c6e93
tolla <anatolii.web@gmail.com>
2016-02-19 02:18:46 +0200

    Использование шаблона для вывода данных

--- app/code/local/DS/News/controllers/IndexController.php
+++ app/code/local/DS/News/controllers/IndexController.php
@@ -10,7 +10,8 @@ class DS_News_IndexController extends Mage_Core_Controller_Front_Action
 
     public function indexAction()
     {
-        echo '<h1>News</h1>';
+        $this->loadLayout();
+        $this->renderLayout();
     }
 
 }
